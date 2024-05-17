1e38462f5599a38526cd1285e82b82d2d622891f
edyi13 <emz19.com@gmail.com>
2024-05-17 13:49:12 -0600

    wordsearch done

--- WordFinderQu/Program.cs
+++ WordFinderQu/Program.cs
@@ -1,205 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Reflection.Emit;
 using System;
+using WordFinderQu;
 
 public class Program
 {
-    public static IEnumerable<string> Find(IEnumerable<string> matrix, IEnumerable<string> wordstream)
-    {
-        var words = new List<string>();
-        words.AddRange(FindHorizontal(matrix, wordstream));
-        words.Add("-----------------------------------------");
-        words.Add("-----------------------------------------");
-        words.Add("-----------------------------------------");
-        words.Add("-----------------------------------------");
-        words.AddRange(FindVertical(matrix, wordstream));
-        return words;
-    }
-
-    public static IEnumerable<string> FindHorizontal(IEnumerable<string> matrix, IEnumerable<string> wordstream)
-    {
-        var words = new List<string>();
-        var matrixArray = matrix.ToArray();
-        var wordstreamArray = wordstream.ToArray();
-        string wordFound = "";
-        bool found = false;
-        int index = 0;
-        for (int m = 0; m < matrixArray.Length; m++)
-        {
-            for (int i = 0; i < wordstreamArray.Length; i++)
-            {
-                string row = matrixArray[m].ToLower().Trim();
-                string word = wordstreamArray[i].ToLower();
-                for (int j = 0; j < row.Length; j++)
-                {
-                    if (word.Length == index)
-                    {
-                        index = 0;
-                    }
-                    else if (word[index] == row[j])
-                    {
-                        wordFound += row[j];
-                        index++;
-                        found = true;
-                        if (wordFound == word)
-                        {
-                            words.Add(wordFound);
-                            wordFound = "";
-                            index = 0;
-                        }
-                    }
-                    else
-                    {
-                        wordFound = "";
-                        if (found)
-                        {
-                            index = 0;
-                            found = false;
-                        }
-                    }
-                }
-            }
-        }
-        return words;
-    }
-
-    public static string[] GetColumn(IEnumerable<string> matrix, int x)
-    {
-        var column = new List<string>();
-        var matrixArray = matrix.ToArray();
-        for (int i = 0; i < matrixArray.Length; i++)
-        {
-            column.Add(matrixArray[i][x].ToString());
-        }
-        return column.ToArray();
-    }
-
-    public static IEnumerable<string> FindVertical(IEnumerable<string> matrix, IEnumerable<string> wordstream)
-    {
-        List<string> words = new List<string>();
-        string[] matrixArray = matrix.ToArray();
-        string[] wordstreamArray = wordstream.ToArray();
-        string wordFound = "";
-        bool found = false;
-        int index = 0;
-        for (int m = 0; m < matrixArray.Length; m++)
-        {
-            for (int i = 0; i < wordstreamArray.Length; i++)
-            {
-                string[] column = GetColumn(matrixArray, m);
-                string word = wordstreamArray[i].ToLower();
-                for (int j = 0; j < column.Length; j++)
-                {
-                    if (word.Length == index)
-                    {
-                        index = 0;
-                    }
-                    else if (word[index].ToString() == column[j].ToLower().Trim())
-                    {
-                        wordFound += column[j].ToLower().Trim();
-                        index++;
-                        found = true;
-                        if (wordFound == word)
-                        {
-                            words.Add(wordFound);
-                            wordFound = "";
-                            index = 0;
-                        }
-                    }
-                    else
-                    {
-                        wordFound = "";
-                        if (found)
-                        {
-                            index = 0;
-                            found = false;
-                        }
-                    }
-                }
-            }
-        }
-        return words;
-    }
-
-    /// <summary>
-    /* This method takes a list of words as input and generates a word matrix. It follows these steps:
-       Initializes a random number generator.
-       Creates a 2D character array matrix to represent the word matrix.
-       Initializes the matrix with spaces ' ' to represent empty cells.
-       Loops through each word in the input list and tries to place the word in a random direction (horizontal or vertical) within the matrix.
-       If a valid position is found, the word is placed in the matrix.
-       After placing all words, fills the remaining empty cells with random letters.
-    */
-    /// </summary>
-    /// <param name="words">List of words for creating a word search matrix</param>
-    /// <param name="matrixSize">Size of the matrix: if you enter 15, the matrix will be 15x15.</param>
-    /// <returns></returns>
-    public static IEnumerable<string> GenerateMatrix(List<string> words, int matrixSize)
-    {
-        Random random = new Random();
-        char[][] matrix = new char[matrixSize][];
-        //Initializes each row of the matrix with spaces ' ', representing empty cells.
-        for (int i = 0; i < matrixSize; i++)
-        {
-            matrix[i] = Enumerable.Repeat(' ', matrixSize).Select(c => (char)c).ToArray();
-        }
-
-        //Iterates through each word in the input list.
-        foreach (string word in words)
-        {
-            bool placed = false;
-            while (!placed)
-            {
-                // Randomly choose direction, row, and column for word placement
-                // Check if word can be placed in the selected position
-                // If valid, place the word and set placed to true
-                int direction = random.Next(2); // 0 for horizontal, 1 for vertical
-                int row = random.Next(matrixSize); // Randomly select a row
-                int column = random.Next(matrixSize - word.Length + 1); // Randomly select a column within the word's length
-
-                if (direction == 0) // Horizontal placement
-                {
-                    // Check if word can fit horizontally and positions are empty
-                    if (column >= 0 && column + word.Length <= matrixSize && matrix[row].Skip(column).Take(word.Length).All(c => c == ' '))
-                    {
-                        // Place the word horizontally in the matrix
-                        for (int i = 0; i < word.Length; i++)
-                        {
-                            matrix[row][column + i] = word[i];
-                        }
-                        placed = true;
-                    }
-                }
-                else // Vertical placement
-                {
-                    // Check if word can fit vertically and positions are empty
-                    if (row + word.Length <= matrixSize && Enumerable.Range(0, word.Length).All(i => matrix[row + i][column] == ' '))
-                    {
-                        // Place the word vertically in the matrix
-                        for (int i = 0; i < word.Length; i++)
-                        {
-                            matrix[row + i][column] = word[i];
-                        }
-                        placed = true;
-                    }
-                }
-            }
-        }
-
-        // Fill remaining empty cells with random letters
-        for (int i = 0; i < matrixSize; i++)
-        {
-            for (int j = 0; j < matrixSize; j++)
-            {
-                if (matrix[i][j] == ' ')
-                {
-                    matrix[i][j] = (char)('A' + random.Next(26)); // Random letter
-                }
-            }
-        }
-
-        return matrix.Select(row => new string(row)).ToList();
-    }
 
 
     public static void Main(String[] args)
@@ -241,13 +46,25 @@ public class Program
             "GOAT",
         };
 
-        IEnumerable<string> result = Find(matrix, wordstream);
+        Solution original = new Solution();
+
+        IEnumerable<string> result = original.Find(matrix, wordstream);
 
         foreach (var item in result)
         {
-            //Console.WriteLine(item);
+            Console.WriteLine(item);
         }
-        //result should be:
+        SolutionOptimized optimized = new SolutionOptimized();
+
+        Console.WriteLine("/****************************************************/");
+
+        IEnumerable<string> resultOptimized = optimized.Find(matrix, wordstream);
+
+        foreach (var item in resultOptimized)
+        {
+            Console.WriteLine(item);
+        }
+        //sample output:
         /*"ALLIGATOR", 1
             "BEAR", 3
             "INCINEROAR", 5
@@ -264,24 +81,11 @@ public class Program
             "GOAT", 2
         */
 
-        //string[] matrix2 = GetColumn(matrix,0);
-        //for (int i = 0; i < matrix2.Length; i++)
+        //var matrixOptimized = optimized.GenerateMatrixOptimized(wordstream, 15);
+        //foreach (var item in matrixOptimized)
         //{
-        //    Console.WriteLine(matrix2[i][0]);
+        //    Console.WriteLine(item);
         //}
-
-        List<string> words = new List<string>
-        {
-            "ALLIGATOR", "BEAR", "INCINEROAR", "ELEPHANT", "FROG", "GIRAFFE",
-            "LION", "MONKEY", "PANDA", "PENGUIN", "TOAD", "POLARBEAR",
-            "TIGER", "GOAT"
-        };
-
-        IEnumerable<string> matrix2 = GenerateMatrix(words, 15);
-
-        foreach (string row in matrix2)
-        {
-            Console.WriteLine(row);
-        }
+        Console.WriteLine("/****************************************************/");
     }
 }
---
+++ WordFinderQu/Solution.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFinderQu
+{
+    public class Solution
+    {
+        /// <summary>
+        /// This method takes a matrix and a wordstream as input and returns the list of the 10 more repeated words found in the matrix.
+        /// </summary>
+        /// <param name="matrix">Is an array of strings representing the matrix.</param>
+        /// <param name="wordstream">Is an array of strings representing the words to search for.</param>
+        /// <returns></returns>
+        public IEnumerable<string> Find(IEnumerable<string> matrix, IEnumerable<string> wordstream)
+        {
+            // Run FindHorizontal and FindVertical in parallel
+            var horizontalTask = Task.Run(() => FindHorizontal(matrix.ToArray(), wordstream.ToArray()));
+            var verticalTask = Task.Run(() => FindVertical(matrix.ToArray(), wordstream.ToArray()));
+
+            Task.WaitAll(horizontalTask, verticalTask);
+
+            var words = new List<string>();
+            words.AddRange(horizontalTask.Result);
+            words.AddRange(verticalTask.Result);
+
+            //return the 10 most repeated words
+            return words.GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Take(10)
+                .Select(x => $"{x.Count()}x {x.Key}");
+        }
+
+        /// <summary>
+        /// This method takes a matrix and a wordstream as input and returns a list of words found horizontally in the matrix.
+        /// </summary>
+        /// <param name="matrix">Is an array of strings representing the matrix.</param>
+        /// <param name="wordstream">Is an array of strings representing the words to search for.</param>
+        /// <returns>return the list of words found horizontally</returns>
+        public static IEnumerable<string> FindHorizontal(string[] matrixArray, string[] wordstreamArray)
+        {
+            var words = new List<string>();
+            string wordFound = "";
+            bool found = false;
+            int index = 0;
+            for (int m = 0; m < matrixArray.Length; m++)
+            {
+                for (int i = 0; i < wordstreamArray.Length; i++)
+                {
+                    string row = matrixArray[m].ToLower().Trim();
+                    string word = wordstreamArray[i].ToLower();
+                    for (int j = 0; j < row.Length; j++)
+                    {
+                        if (word.Length == index)
+                        {
+                            index = 0;
+                        }
+                        else if (word[index] == row[j])
+                        {
+                            wordFound += row[j];
+                            index++;
+                            found = true;
+                            if (wordFound == word)
+                            {
+                                words.Add(wordFound);
+                                wordFound = "";
+                                index = 0;
+                            }
+                        }
+                        else
+                        {
+                            wordFound = "";
+                            if (found)
+                            {
+                                index = 0;
+                                found = false;
+                            }
+                        }
+                    }
+                }
+            }
+            return words;
+        }
+
+        /// <summary>
+        /// This method takes a matrix and a column index as input and returns the column as an array of strings.
+        /// </summary>
+        /// <param name="matrix">Is an array of strings</param>
+        /// <param name="index">Is the column index </param>
+        /// <returns>return column as an array of strings</returns>
+        public static string[] GetColumn(string[] matrix, int index)
+        {
+            return matrix.Select(row => row[index].ToString()).ToArray();
+        }
+
+        /// <summary>
+        /// This method takes a matrix and a wordstream as input and returns a list of words found vertically in the matrix.
+        /// </summary>
+        /// <param name="matrix">Is an array of strings representing the matrix.</param>
+        /// <param name="wordstream">Is an array of strings representing the words to search for.</param>
+        /// <returns>return the list of words found vertically</returns>
+        public static IEnumerable<string> FindVertical(string[] matrixArray, string[] wordstreamArray)
+        {
+            List<string> words = new List<string>();
+            string wordFound = "";
+            bool found = false;
+            int index = 0;
+            for (int m = 0; m < matrixArray.Length; m++)
+            {
+                for (int i = 0; i < wordstreamArray.Length; i++)
+                {
+                    string[] column = GetColumn(matrixArray, m);
+                    string word = wordstreamArray[i].ToLower();
+                    for (int j = 0; j < column.Length; j++)
+                    {
+                        if (word.Length == index)
+                        {
+                            index = 0;
+                        }
+                        else if (word[index].ToString() == column[j].ToLower().Trim())
+                        {
+                            wordFound += column[j].ToLower().Trim();
+                            index++;
+                            found = true;
+                            if (wordFound == word)
+                            {
+                                words.Add(wordFound);
+                                wordFound = "";
+                                index = 0;
+                            }
+                        }
+                        else
+                        {
+                            wordFound = "";
+                            if (found)
+                            {
+                                index = 0;
+                                found = false;
+                            }
+                        }
+                    }
+                }
+            }
+            return words;
+        }
+
+        /// <summary>
+        /* This method takes a list of words as input and generates a word matrix. It follows these steps:
+           Initializes a random number generator.
+           Creates a 2D character array matrix to represent the word matrix.
+           Initializes the matrix with spaces ' ' to represent empty cells.
+           Loops through each word in the input list and tries to place the word in a random direction (horizontal or vertical) within the matrix.
+           If a valid position is found, the word is placed in the matrix.
+           After placing all words, fills the remaining empty cells with random letters.
+        */
+        /// </summary>
+        /// <param name="words">List of words for creating a word search matrix</param>
+        /// <param name="matrixSize">Size of the matrix: if you enter 15, the matrix will be 15x15.</param>
+        /// <returns></returns>
+        public static IEnumerable<string> GenerateMatrix(List<string> words, int matrixSize)
+        {
+            Random random = new Random();
+            char[][] matrix = new char[matrixSize][];
+            //Initializes each row of the matrix with spaces ' ', representing empty cells.
+            for (int i = 0; i < matrixSize; i++)
+            {
+                matrix[i] = Enumerable.Repeat(' ', matrixSize).Select(c => (char)c).ToArray();
+            }
+
+            //Iterates through each word in the input list.
+            foreach (string word in words)
+            {
+                bool placed = false;
+                while (!placed)
+                {
+                    // Randomly choose direction, row, and column for word placement
+                    // Check if word can be placed in the selected position
+                    // If valid, place the word and set placed to true
+                    int direction = random.Next(2); // 0 for horizontal, 1 for vertical
+                    int row = random.Next(matrixSize); // Randomly select a row
+                    int column = random.Next(matrixSize - word.Length + 1); // Randomly select a column within the word's length
+
+                    if (direction == 0) // Horizontal placement
+                    {
+                        // Check if word can fit horizontally and positions are empty
+                        if (column >= 0 && column + word.Length <= matrixSize && matrix[row].Skip(column).Take(word.Length).All(c => c == ' '))
+                        {
+                            // Place the word horizontally in the matrix
+                            for (int i = 0; i < word.Length; i++)
+                            {
+                                matrix[row][column + i] = word[i];
+                            }
+                            placed = true;
+                        }
+                    }
+                    else // Vertical placement
+                    {
+                        // Check if word can fit vertically and positions are empty
+                        if (row + word.Length <= matrixSize && Enumerable.Range(0, word.Length).All(i => matrix[row + i][column] == ' '))
+                        {
+                            // Place the word vertically in the matrix
+                            for (int i = 0; i < word.Length; i++)
+                            {
+                                matrix[row + i][column] = word[i];
+                            }
+                            placed = true;
+                        }
+                    }
+                }
+            }
+
+            // Fill remaining empty cells with random letters
+            for (int i = 0; i < matrixSize; i++)
+            {
+                for (int j = 0; j < matrixSize; j++)
+                {
+                    if (matrix[i][j] == ' ')
+                    {
+                        matrix[i][j] = (char)('A' + random.Next(26)); // Random letter
+                    }
+                }
+            }
+
+            return matrix.Select(row => new string(row)).ToList();
+        }
+
+    }
+}
